798dbda913675e18d5e075cf22943ad5825c90d8
anishchaudhary27 <anishchaudhary2706@gmail.com>
2020-09-29 00:30:03 +0530

    added sign in

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import Toolbar from '@material-ui/core/Toolbar';
+import firebase from "firebase/app"
+import "firebase/auth"
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  appbar: {
+
+  },
+  title: {
+    flexGrow: 1,
+  },
+  loginBtn: {
+
+  },
+})
 
 function App() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(false);
+  React.useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        setAuth(true);
+      }
+      else {
+        setAuth(false);
+      }
+    })
+  }, []);
+
+  const signIn = () => {
+    if (auth) {
+      firebase.auth().signOut().catch(err=>console.error(err));
+    }
+    else {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider).then(value => {
+        console.log('user signed in');
+      })
+        .catch(err => {
+          console.error(err)
+        });
+    }
+  }
+
   return (
-    <div className="App">
-      <h1>Web Technology Project</h1>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Dashboard
+          </Typography>
+          <Button color="secondary" variant="contained" onClick={signIn}>{auth ? 'Sign Out' : 'sign in'}</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
