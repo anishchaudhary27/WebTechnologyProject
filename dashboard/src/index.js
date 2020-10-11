73f1b8f3d72b8076a250d0d7d6f6304e9f4121fc
anishchaudhary27 <anishchaudhary2706@gmail.com>
2020-10-11 21:30:55 +0530

    bdknaks

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -3,24 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import firebase from 'firebase/app';
 
-// var firebaseConfig = {
-//   apiKey: 'AIzaSyAdDltso982aXGaKayEEsb8X8QrrCBPuYM',
-//   authDomain: 'webtechonologyproject.firebaseapp.com',
-//   databaseURL: 'https://webtechonologyproject.firebaseio.com',
-//   projectId: 'webtechonologyproject',
-//   storageBucket: 'webtechonologyproject.appspot.com',
-//   messagingSenderId: '203786217273',
-//   appId: '1:203786217273:web:4f8336e3c15a5efacbeae8'
-// };
 var firebaseConfig = {
-  apiKey: 'AIzaSyCTbdwnd9yUHByA2JKyma36ZRLPk393JXk',
-  authDomain: 'webtechnology-project.firebaseapp.com',
-  databaseURL: 'https://webtechnology-project.firebaseio.com',
-  projectId: 'webtechnology-project',
-  storageBucket: 'webtechnology-project.appspot.com',
-  messagingSenderId: '744967695135',
-  appId: '1:744967695135:web:a2347c636a4aa367195e2a'
+  apiKey: 'AIzaSyAdDltso982aXGaKayEEsb8X8QrrCBPuYM',
+  authDomain: 'webtechonologyproject.firebaseapp.com',
+  databaseURL: 'https://webtechonologyproject.firebaseio.com',
+  projectId: 'webtechonologyproject',
+  storageBucket: 'webtechonologyproject.appspot.com',
+  messagingSenderId: '203786217273',
+  appId: '1:203786217273:web:4f8336e3c15a5efacbeae8'
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
